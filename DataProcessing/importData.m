941eb2101c76907f7defc0a9ac8717fc2d77d720
Ben Fulcher <ben.d.fulcher@gmail.com>
2017-06-14 13:23:27 +1000

    Working up data import

--- DataProcessing/importData.m
+++ DataProcessing/importData.m
@@ -1,10 +1,29 @@
+%-------------------------------------------------------------------------------
+% Do the importing from csv files:
+%-------------------------------------------------------------------------------
+fprintf(1,'Importing data from csv files\n');
 eQTLedges = importEdges();
 eQTLidentifier = importIdentifier();
 eQTLproteinnames = importProteinNames();
 
+%-------------------------------------------------------------------------------
+numInteractions = height(eQTLedges);
 numProteins = length(eQTLproteinnames);
 
-fprintf(1,'Constructing a %ux%u protein-protein interaction network\n',...
-                    numProteins,numProteins);
+fprintf(1,['Constructing a %ux%u protein-protein interaction network',...
+                ' containing %u interactions...\n'],...
+                    numProteins,numProteins,numInteractions);
 
 Adj = zeros(numProteins,numProteins);
+
+for k = 1:numInteractions
+    ii = strcmp(eQTLproteinnames,eQTLedges.InteractorA(k));
+    jj = strcmp(eQTLproteinnames,eQTLedges.InteractorB(k));
+    Adj(ii,jj) = 1;
+end
+
+
+%-------------------------------------------------------------------------------
+% Save to file
+%-------------------------------------------------------------------------------
+save(fullfile('Data','processedData.mat'),'Adj','eQTLproteinnames');
