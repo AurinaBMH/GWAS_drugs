dd71b38b8f3d45dffa60d903777fd7aceeaee796
Ben Fulcher <ben.d.fulcher@gmail.com>
2017-09-26 22:55:25 +1000

    Working up pipeline

--- Analysis/GeneDrugCharacterization/pipeline.m
+++ Analysis/GeneDrugCharacterization/pipeline.m
@@ -16,9 +16,9 @@ geneName = C{1}; drugName = C{2};
 geneDrug = table(geneName,drugName);
 allUniqueGenes = unique(geneDrug.geneName);
 numUniqueGenes = length(allUniqueGenes);
-fprintf(1,'%u unique genes with drug targets (in Drugbank)\n',numUniqueGenes);
+fprintf(1,'%u genes have drug targets in Drugbank\n',numUniqueGenes);
 numUniqueDrugs = length(unique(geneDrug.drugName));
-fprintf(1,'%u unique drugs with gene targets (in Drugbank)\n',numUniqueDrugs);
+fprintf(1,'%u drugs have gene targets in Drugbank\n',numUniqueDrugs);
 
 %-------------------------------------------------------------------------------
 % Read in SNP annotations
@@ -47,12 +47,12 @@ SNPGeneMap = SNPAnnotationTable(cellfun(@(x)~isempty(x),SNPAnnotationTable.mappe
 % Make unique:
 [~,ix] = unique(SNPGeneMap.SNP_id);
 SNPGeneMap = SNPGeneMap(ix,:);
+fprintf(1,'%u/%u SNPs have a matching gene name (%u genes)\n',height(SNPGeneMap),...
+        length(unique(SNPAnnotationTable.SNP_id)),length(unique(SNPGeneMap.mappedGeneSNP)));
 
 %-------------------------------------------------------------------------------
-% Infer the LD gene (i.e., the gene causing the annotation) for LD annotations
-% in SNPAnnotationTable
-%-------------------------------------------------------------------------------
 % Load the LD relationship data:
+%-------------------------------------------------------------------------------
 fid = fopen('2_2_LD_r2.csv','r');
 C = textscan(fid,'%s%f%s','Delimiter',',','HeaderLines',1);
 fclose(fid);
@@ -60,40 +60,54 @@ SNP_id_1 = C{1};
 SNP_id_2 = C{3};
 LD = C{2};
 LDRelateTable = table(SNP_id_1,SNP_id_2,LD);
+%-------------------------------------------------------------------------------
+
+%-------------------------------------------------------------------------------
+% Infer the LD gene (i.e., the gene causing the annotation) for LD annotations
+% in SNPAnnotationTable
+%-------------------------------------------------------------------------------
 
 % For LD SNPs, we can annotate the SNP that caused the annotation
-LDGene = cell(numAnnotations,1);
-for i = 1:numAnnotations
-    if SNPAnnotationTable.isLD(i)
-        % We want to determine which gene caused the annotation:
-        theLDSNP1 = LDRelateTable.SNP_id_2(strcmp(LDRelateTable.SNP_id_1,SNPAnnotationTable.SNP_id{i}));
-        theLDSNP2 = LDRelateTable.SNP_id_1(strcmp(LDRelateTable.SNP_id_2,SNPAnnotationTable.SNP_id{i}));
-        theLDSNPs = vertcat(theLDSNP1{:},theLDSNP2{:});
-        if ischar(theLDSNPs), theLDSNPs = {theLDSNPs}; end
-
-        % Match to genes:
-        theLDGenes = cellfun(@(x)SNPGeneMap.mappedGeneSNP(strcmp(SNPGeneMap.SNP_id,x)),theLDSNPs,'UniformOutput',false);
-
-        if isempty(theLDGenes)
-            warning('No LD gene for %s',SNPAnnotationTable.SNP_id{i})
-            LDGene{i} = '';
-        elseif
-            
-        end
-
-    else
-        LDGene{i} = '';
-    end
-end
+% LDGene = cell(numAnnotations,1);
+% for i = 1:numAnnotations
+%     if SNPAnnotationTable.isLD(i)
+%         % We want to determine which gene caused the annotation:
+%         theLDSNP1 = LDRelateTable.SNP_id_2(strcmp(LDRelateTable.SNP_id_1,SNPAnnotationTable.SNP_id{i}));
+%         theLDSNP2 = LDRelateTable.SNP_id_1(strcmp(LDRelateTable.SNP_id_2,SNPAnnotationTable.SNP_id{i}));
+%         theLDSNPs = unique(vertcat(theLDSNP1,theLDSNP2));
+%         if ischar(theLDSNPs), theLDSNPs = {theLDSNPs}; end
+%
+%         % Match to genes:
+%         theLDGenes = cellfun(@(x)SNPGeneMap.mappedGeneSNP(strcmp(SNPGeneMap.SNP_id,x)),theLDSNPs,'UniformOutput',false);
+%
+%         if isempty(theLDGenes)
+%             % No matches? Must be an LD relationship to a SNP without a gene
+%             warning('No LD gene for %s',SNPAnnotationTable.SNP_id{i});
+%             LDGene{i} = '';
+%         elseif length(theLDGenes)==1
+%             % One match -- easy:
+%             LDGene{i} = theLDGenes{1};
+%         else
+%             fprintf(1,'Multiple matches :-O\n');
+%         end
+%
+%     else
+%         LDGene{i} = '';
+%     end
+% end
 
 %-------------------------------------------------------------------------------
 % Now loop through genes to characterize...
 numGWASMapped = zeros(numUniqueGenes,1);
+numLDSNPs = zeros(numUniqueGenes,1);
 
 for i = 1:numUniqueGenes
-    fprintf(1,'[%u/%u]: %s\n',i,numUniqueGenes,allUniqueGenes{i});
+    gene_i = allUniqueGenes{i};
+    fprintf(1,'[%u/%u]: %s\n',i,numUniqueGenes,gene_i);
+    %-------------------------------------------------------------------------------
     % --numGWASMapped: the number of GWAS SNPs mapped directly to a gene
-    theSNPs = SNPAnnotationTable.SNP_id(strcmp(SNPAnnotationTable.mappedGeneSNP,allUniqueGenes{i}));
+    %-------------------------------------------------------------------------------
+    theSNPs = SNPAnnotationTable.SNP_id(strcmp(SNPAnnotationTable.mappedGeneSNP,gene_i));
     if isempty(theSNPs)
         numGWASMapped(i) = 0;
     else
@@ -101,5 +115,17 @@ for i = 1:numUniqueGenes
     end
 
     %-------------------------------------------------------------------------------
+    % --numLD: the number of GWAS SNPs LD to a gene
+    %-------------------------------------------------------------------------------
+    % If I want to count the number of SNPs that are LD to a given gene, I need to:
+    % (i) Check every SNP in that gene (?? Using SNP_identifier.csv??)
+    % (ii) Get a list of all (unique) SNPs are LD to these SNPs (Using LD_r2.csv)
+    % (iii) Check all LD SNPs for direct disease annotations (in SNP_identifier.csv).
 
+    numLDSNPs(i) = CountLDSNPs(gene_i,SNPAnnotationTable,LDRelateTable,SNPGeneMap);
 end
+
+% Now make a table
+gene = allUniqueGenes;
+results = table(gene,numGWASMapped,numLDSNPs);
+results = sortrows(results,'numGWASMapped','descend');
