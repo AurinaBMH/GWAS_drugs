98cb4d02aba5b08f3b4eb881505f80a31981447d
Ben Fulcher <ben.d.fulcher@gmail.com>
2017-09-26 15:06:50 +1000

    Restructuring for gene characterization analysis

---
+++ Analysis/GeneDrugCharacterization/pipeline.m
@@ -0,0 +1,6 @@
+% Pipeline for producing table characterizing individual genes
+
+%-------------------------------------------------------------------------------
+% Get genes
+%-------------------------------------------------------------------------------
+% List of all genes with drug targets in Drugbank are in **gene_ATC_matrix.csv**
