d3fd6c06d9c9e897a51007ab3f8e1604184aeba2
Ben Fulcher <ben.d.fulcher@gmail.com>
2017-09-26 15:15:05 +1000

    Load in genes with drugs targets

--- Analysis/GeneDrugCharacterization/pipeline.m
+++ Analysis/GeneDrugCharacterization/pipeline.m
@@ -4,3 +4,18 @@
 % Get genes
 %-------------------------------------------------------------------------------
 % List of all genes with drug targets in Drugbank are in **gene_ATC_matrix.csv**
+
+fid = fopen('8_1_drug_gene.csv','r');
+C = textscan(fid,'%s%s','Delimiter',',');
+fclose(fid)
+geneName = C{1}; drugName = C{2};
+geneDrug = table(geneName,drugName);
+allUniqueGenes = unique(geneDrug.geneName);
+numUniqueGenes = length(allUniqueGenes);
+fprintf(1,'%u unique genes with drug targets (in Drugbank)\n',numUniqueGenes);
+numUniqueDrugs = length(unique(geneDrug.drugName));
+fprintf(1,'%u unique drugs with gene targets (in Drugbank)\n',numUniqueDrugs);
+
+
+
+%-------------------------------------------------------------------------------
