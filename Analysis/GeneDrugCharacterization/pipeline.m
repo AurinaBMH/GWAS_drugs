33ac7b8ce922d66b73b1a3b947b7f0183242b5dd
Ben Fulcher <ben.d.fulcher@gmail.com>
2017-10-09 16:25:16 +1100

    Updated counting of LD SNPs

--- Analysis/GeneDrugCharacterization/pipeline.m
+++ Analysis/GeneDrugCharacterization/pipeline.m
@@ -95,11 +95,12 @@ for i = 1:numUniqueGenes
     %-------------------------------------------------------------------------------
     % --numGWASMapped: the number of GWAS SNPs mapped directly to a gene
     %-------------------------------------------------------------------------------
-    theSNPs = SNPAnnotationTable.SNP_id(strcmp(SNPAnnotationTable.mappedGene,gene_i) & SNPAnnotationTable.isGWAS);
-    if isempty(theSNPs)
+    isGeneAndMapped = strcmp(SNPAnnotationTable.mappedGene,gene_i) & SNPAnnotationTable.isGWAS;
+    theMappedSNPs = unique(SNPAnnotationTable.SNP_id(isGeneAndMapped));
+    if isempty(theMappedSNPs)
         numGWASMapped(i) = 0;
     else
-        numGWASMapped(i) = length(unique(theSNPs));
+        numGWASMapped(i) = length(theMappedSNPs);
     end
 
     %-------------------------------------------------------------------------------
@@ -110,9 +111,18 @@ for i = 1:numUniqueGenes
     % (ii) Get a list of all (unique) SNPs are LD to these SNPs (Using LD_r2.csv)
     % (iii) Check all LD SNPs for direct disease annotations (in SNP_identifier.csv).
     isGeneAndLD = strcmp(SNPAnnotationTable.mappedGene,gene_i) & SNPAnnotationTable.isLD;
+    theLDSNPs = unique(SNPAnnotationTable.SNP_id(isGeneAndLD));
     % NB: No way to double check that all LD annotations are from unique SNPs...
     % (is an assumption that Janette has formatted the file in this way)
-    numLDSNPs(i) = sum(isGeneAndLD);
+
+    % Instead, we count all unique SNPs that are in a gene and LD to some GWAS hit:
+    if isempty(theLDSNPs)
+        numLDSNPs(i) = 0;
+    else
+        % Exclude SNPs that are mapped directly
+        inBoth = intersect(theMappedSNPs,theLDSNPs);
+        numLDSNPs(i) = length(theLDSNPs) - length(inBoth);
+    end
 
     %-------------------------------------------------------------------------------
     % eQTLs-GWAS
