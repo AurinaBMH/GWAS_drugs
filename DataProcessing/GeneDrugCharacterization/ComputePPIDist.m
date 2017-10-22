0f50282a01f31d67a011349a64e36373531a5dc7
Ben Fulcher <ben.d.fulcher@gmail.com>
2017-10-22 14:51:18 +1100

    Computing PPI distances

---
+++ DataProcessing/GeneDrugCharacterization/ComputePPIDist.m
@@ -0,0 +1,21 @@
+function distMatrix = ComputePPIDist(evidenceThreshold)
+% Import PPIN data from STRING
+%-------------------------------------------------------------------------------
+if nargin < 1
+    evidenceThreshold = 0;
+end
+fileNameLoad = sprintf('PPI_Adj_th%u.mat',evidenceThreshold);
+fileNameSave = sprintf('PPI_Dist_th%u.mat',evidenceThreshold);
+load(fileNameLoad,'AdjPPI')
+%-------------------------------------------------------------------------------
+
+%-------------------------------------------------------------------------------
+% Compute pairwise distances:
+fprintf(1,'Computing pairwise distances between %u genes\n',size(AdjPPI,1));
+distMatrix = distance_bin(AdjPPI);
+
+%-------------------------------------------------------------------------------
+% Save out:
+save(fullfile('Data',fileNameSave),'distMatrix');
+
+end
