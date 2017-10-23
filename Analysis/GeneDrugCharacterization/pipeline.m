dcfbaa96662b45a8f54f32b74fce142b5a268589
Ben Fulcher <ben.d.fulcher@gmail.com>
2017-10-23 16:23:58 +1100

    Added simple PPI measures

--- Analysis/GeneDrugCharacterization/pipeline.m
+++ Analysis/GeneDrugCharacterization/pipeline.m
@@ -53,7 +53,9 @@ catch
     warning('No PPI shortest path information found');
 end
 % Get indices of mapped disease genes on PPI network:
-PPI_isDiseaseGeneMapped = cellfun(@(x)find(strcmp(x,PPIN.geneNames)),allDiseaseGenesMapped);
+PPI_isDiseaseGeneMapped = ismember(PPIN.geneNames,allDiseaseGenesMapped);
+fprintf(1,'%u/%u disease genes are in the PPI network\n',sum(PPI_isDiseaseGeneMapped),...
+                                                        length(allDiseaseGenesMapped));
 
 %-------------------------------------------------------------------------------
 % Now loop through genes to characterize...
@@ -65,8 +67,10 @@ numEGenes_LD = zeros(numUniqueGenes,1);
 numSNPGenes_LD = zeros(numUniqueGenes,1);
 numLDeGeneseQTL = zeros(numUniqueGenes,1);
 numLDSNPGeneseQTL = zeros(numUniqueGenes,1);
-numPPIneighborsDiseaseMapped = zeros(numUniqueGenes,1);
-numPPIneighborsDiseaseLD = zeros(numUniqueGenes,1);
+numPPIneighbors1DiseaseMapped = zeros(numUniqueGenes,1);
+numPPIneighbors1DiseaseLD = zeros(numUniqueGenes,1);
+propPPIneighbors1DiseaseMapped = zeros(numUniqueGenes,1);
+propPPIneighbors1DiseaseLD = zeros(numUniqueGenes,1);
 meanPPIDistance = zeros(numUniqueGenes,1);
 matchingDrugsString = cell(numUniqueGenes,1);
 
@@ -185,13 +189,29 @@ for i = 1:numUniqueGenes
     %-------------------------------------------------------------------------------
     % PPIN Neighbors
     %-------------------------------------------------------------------------------
-    % Genes that are neighbors on the PPI network:
+    % Count disease genes that are 1-step neighbors on the PPI network:
     PPI_index = strcmp(PPIN.geneNames,gene_i);
-    PPI_neighbors_index = PPIN.AdjPPI(PPI_index,:);
+    PPI_neighbors_index = PPIN.AdjPPI(PPI_index,:); % the 1-step neighbors
+    PPI_neighbors_index = union(PPI_neighbors_index,PPI_index); % include the target
     PPI_neighbors_gene = PPIN.geneNames(PPI_neighbors_index);
-    % How many first degree neighbors are in the disease list (mapped/LD)?:
-    numPPIneighborsDiseaseMapped(i) = sum(ismember(PPI_neighbors_gene,allDiseaseGenesMapped));
-    numPPIneighborsDiseaseLD(i) = sum(ismember(PPI_neighbors_gene,allDiseaseGenesLD));
+    % How many 1-step neighbors are in the disease list (mapped/LD)?:
+    numPPIneighbors1DiseaseMapped(i) = sum(ismember(PPI_neighbors_gene,allDiseaseGenesMapped));
+    numPPIneighbors1DiseaseLD(i) = sum(ismember(PPI_neighbors_gene,allDiseaseGenesLD));
+    % As a proportion of the number of neighbors (~penalizes genes with many neighbors):
+    propPPIneighbors1DiseaseMapped(i) = numPPIneighbors1DiseaseMapped(i)/length(PPI_neighbors_gene);
+    propPPIneighbors1DiseaseLD(i) = numPPIneighbors1DiseaseLD(i)/length(PPI_neighbors_gene);
+
+    %===============================================================================
+    % Genes that are 2-step neighbors on the PPI network -- doesn't make much sense (so many!):
+    % (e.g., in one example case there are 3235 1-step neighbors; 17992 2-step neighbors)
+    % (this basically gives coverage to the entire network in two steps...)
+    % neighborsIndex = find(PPI_neighbors_index); % list of indices
+    % neighborNeighbors = arrayfun(@(x)find(PPIN.AdjPPI(x,:)),neighborsIndex,'UniformOutput',false);
+    % twoStepNeighbors = unique(horzcat(neighborNeighbors{:}));
+    % PPI_neighbors_gene = PPIN.geneNames(PPI_neighbors_index);
+    % % How many 1/2-step neighbors are in the disease list (mapped/LD)?:
+    % numPPIneighbors2DiseaseMapped(i) = sum(ismember(PPI_neighbors_gene,allDiseaseGenesMapped));
+    % numPPIneighbors2DiseaseLD(i) = sum(ismember(PPI_neighbors_gene,allDiseaseGenesLD));
 
     %-------------------------------------------------------------------------------
     % PPIN Distances
@@ -216,12 +236,13 @@ end
 
 % Now make a table
 gene = allUniqueGenes;
-results = table(gene,numGWASMapped,numLDSNPs,numPPIneighborsDiseaseMapped,numPPIneighborsDiseaseLD,...
+results = table(gene,numGWASMapped,numLDSNPs,numPPIneighbors1DiseaseMapped,numPPIneighbors1DiseaseLD,...
                 meanPPIDistance,numEGenes,numSNPGenes,numEGenes_LD,numSNPGenes_LD,...
                 numLDeGeneseQTL,numLDSNPGeneseQTL,matchingDrugsString);
+
 % Sort by column, then by column, etc. in ordered hierarchy:
-results = sortrows(results,{'numGWASMapped','numLDSNPs','numPPIneighborsDiseaseMapped',...
-                'numPPIneighborsDiseaseLD','meanPPIDistance','numEGenes','numSNPGenes','numEGenes_LD',...
+results = sortrows(results,{'numGWASMapped','numLDSNPs','numPPIneighbors1DiseaseMapped',...
+                'numPPIneighbors1DiseaseLD','meanPPIDistance','numEGenes','numSNPGenes','numEGenes_LD',...
                 'numSNPGenes_LD','numLDeGeneseQTL','numLDSNPGeneseQTL'},'descend');
 fprintf(1,'%u genes have mapped and LD\n',sum(results.numLDSNPs > 0 & results.numGWASMapped > 0));
 fprintf(1,'%u genes have mapped but no LD\n',sum(results.numLDSNPs==0 & results.numGWASMapped > 0));
