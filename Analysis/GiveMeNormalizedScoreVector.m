0f6cbfd63db83cf914257760272b273398644e41
Ben Fulcher <ben.d.fulcher@gmail.com>
2018-09-27 11:26:50 +1000

    Trying to integrate consistent code for generating normalized score vectors

---
+++ Analysis/GiveMeNormalizedScoreVector.m
@@ -0,0 +1,64 @@
+function [geneNames,geneWeightsNorm,geneWeights] = GiveMeNormalizedScoreVector(whatDisease,whatMeasurement,similarityType,whatProperty)
+%-------------------------------------------------------------------------------
+% Inputs:
+%-------------------------------------------------------------------------------
+if nargin < 1
+    whatDisease = 'SZP';
+end
+if nargin < 2
+    whatMeasurement = 'GWAS';
+end
+if nargin < 3
+    similarityType = 'DNA';
+end
+if nargin < 4
+    whatProperty = 'percGWAS';
+end
+
+%-------------------------------------------------------------------------------
+
+switch whatMeasurement
+case 'Drug'
+    normalizeWithinDrugs = true;
+    indicatorTable = ImportTreatmentLists(normalizeWithinDrugs);
+    geneNames = indicatorTable.Row;
+    geneWeights = indicatorTable.(whatDisease);
+case 'GWAS'
+    % Load data:
+    fileName = sprintf('resultsTable_%s.mat',whatDisease);
+    load(fileName,'geneScores');
+    fprintf(1,'Loaded gene scores for %s from %s\n',whatDisease,fileName);
+    geneNames = geneScores.gene;
+    switch similarityType
+    case 'DNA'
+        % numSNPs, numGWAS, percGWAS, numLD, percLD
+        geneWeights = geneScores.DNA.(whatProperty);
+    case 'PPI_weighted'
+        % numPPIneighbors1
+        % percPPIneighbors1
+        % weiPPIneighbors1
+        % medianPPIDistance
+        % meanPPIDistance
+        geneWeights = geneScores.PPI_mapped_weighted.(whatProperty);
+    case 'PPI_th0'
+        geneWeights = geneScores.PPI_mapped_th0.(whatProperty);
+    case 'PPI_th400'
+        geneWeights = geneScores.PPI_mapped_th400.(whatProperty);
+    case 'PPI_th600'
+        geneWeights = geneScores.PPI_mapped_th600.(whatProperty);
+    case 'PPI_th900'
+        geneWeights = geneScores.PPI_mapped_th900.(whatProperty);
+    case 'Expression'
+        geneWeights = geneScores.AllenMeanCoexpMapped;
+    otherwise
+        error('Unknown similarity type: ''%s''',similarityType);
+    end
+end
+
+%-------------------------------------------------------------------------------
+% Normalize (non-NaN elements) to unit vector as 2-norm:
+r = ~isnan(geneWeights);
+geneWeightsNorm = geneWeights;
+geneWeightsNorm(r) = geneWeightsNorm(r)/norm(geneWeights(r),2);
+
+end
--- Analysis/VisualizeWeightingVectors.m
+++ Analysis/VisualizeWeightingVectors.m
@@ -1,24 +1,35 @@
 % Parameters:
+whichDiseases = {'ADHD','MDD','BIP','SZP'}; % 'diabetes','pulmonary','cardiology','gastro'};
+% whatMeasurement = 'Drug';
+whatMeasurement = 'GWAS';
+% similarityType = 'DNA';
+% whatProperty = 'percGWAS';
+similarityType = 'PPI_th0';
+whatProperty = 'percPPIneighbors1';
 numTop = 30; % Look at the top X genes in particular
-normalizeWithinDrugs = true;
-whichDiseases = {'ADHD','MDD','BIP','SZP'};
-% 'diabetes','pulmonary','cardiology','gastro'};
 
 %-------------------------------------------------------------------------------
-indicatorTable = ImportTreatmentLists(normalizeWithinDrugs)
-[~,ia,ib] = intersect(whichDiseases,indicatorTable.Properties.VariableNames,'stable');
-indicatorTable = indicatorTable(:,ib);
-
-%-------------------------------------------------------------------------------
-% Normalize:
-dataNorm = indicatorTable{:,:}';
-for i = 1:4
-    dataNorm(i,:) = dataNorm(i,:)/norm(dataNorm(i,:),2);
+numDiseases = length(whichDiseases);
+geneNames = cell(numDiseases,1);
+geneWeightsNorm = cell(numDiseases,1);
+for i = 1:numDiseases
+    whatDisease = whichDiseases{i};
+    [geneNames{i},geneWeightsNorm{i}] = GiveMeNormalizedScoreVector(whatDisease,...
+                                    whatMeasurement,similarityType,whatProperty);
 end
+
+% Combine multiple diseases:
+% [~,ia,ib] = intersect(whichDiseases,indicatorTable.Properties.VariableNames,'stable');
+% indicatorTable = indicatorTable(:,ib);
+data = horzcat(geneWeightsNorm{:})';
+
 % Order by maximum weight:
-maxGene = max(dataNorm,[],1);
-[~,ix] = sort(maxGene,'descend');
-data = dataNorm(:,ix(1:numTop));
+% maxGene = max(data,[],1);
+maxGene = mean(data,1);
+[maxGeneSort,ix] = sort(maxGene,'descend');
+ix(isnan(maxGeneSort)) = []; % remove top genes that are actually NaNs
+data = data(:,ix(1:numTop));
+geneNames = geneNames{1}(ix(1:numTop));
 
 %-------------------------------------------------------------------------------
 f = figure('color','w');
@@ -26,12 +37,19 @@ imagesc(data)
 colormap([1,1,1;BF_getcmap('blues',9,false)])
 % colormap([1,1,1;jet(12)])
 ax = gca;
-ax.YTick = 1:size(data,1);
-ax.YTickLabel = indicatorTable.Properties.VariableNames;
+ax.YTick = 1:numDiseases;
+ax.YTickLabel = whichDiseases;
 ax.XTick = 1:numTop;
-ax.XTickLabel = indicatorTable.Row(1:numTop);
+ax.XTickLabel = geneNames;
 ax.XTickLabelRotation = 90;
 cB = colorbar;
 cB.Label.String = 'Normalized treatment weight';
-caxis([0,0.4])
-title('Drug weightings over genes')
+caxis([0,0.15])
+% Title:
+extraText = '';
+if strcmp(whatMeasurement,'GWAS')
+    extraText = sprintf('%s-%s',similarityType,whatProperty);
+end
+title(sprintf('%s weightings over genes (%s)',whatMeasurement,extraText),...
+                    'interpreter','none')
+f.Position = [440   427   887   371];
