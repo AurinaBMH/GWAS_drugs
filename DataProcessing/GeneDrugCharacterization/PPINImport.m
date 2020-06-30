a03c906b093449c7e053521c699f862845be1d18
Aurina <aurina.arnatk@gmail.com>
2020-06-30 10:13:12 +1000

    Old file in HGNC replaced with new and used as default Only unique pairs of genes(proteins) selected by keeping one with max evidence score; Duplicate exclusion takes very long time - is there a more efficient way?

--- DataProcessing/GeneDrugCharacterization/PPINImport.m
+++ DataProcessing/GeneDrugCharacterization/PPINImport.m
@@ -6,9 +6,11 @@ function [AdjPPI,geneNames] = PPINImport(doWeighted,evidenceThreshold,whatInput)
 if nargin < 1
     doWeighted = false;
 end
+
 if nargin < 2
     evidenceThreshold = 400;
 end
+
 if nargin < 3
     whatInput = 'HGNCmatch';
 end
@@ -22,15 +24,16 @@ fileNameSave = PPIFileNames(doWeighted,evidenceThreshold,whatInput);
 switch whatInput
     case 'HGNCmatch'
         % Latest matching to HGNC genes by Janette (July 2018):
-        fileName = 'PPI_conversionToGenes.csv';
+        % fileName = 'PPI_conversionToGenes.csv';
+        % File generated based on 9606.protein.links.v11.0.txt (June 2020), gene names extracted from biomart using 
+        % make_PPI_linkfile.m
+        fileName = 'PPIlinks_v11.0.txt'; 
     case 'proteins'
         % Mapping to proteins:
         fileName = '9606.protein.links.v10.5.txt';
     case 'original'
         % Original STRING nomenclature:
         fileName = '6_PPIN_STRINGv10.5.csv';
-    case 'genes'
-        fileName = 'PPIlinks.txt';
 end
 fprintf(1,'Constructing protein-protein interactions using %s input: ''%s''\n',...
                 whatInput,fileName);
@@ -43,11 +46,10 @@ gene2 = C{2};
 evidenceScore = C{3};
 clear('C');
 fprintf(1,'Read in %u interactions :-O\n',length(gene1));
-
-G = [gene1,gene2]; 
-nPairsR = table2cell(unique(cell2table(G))); 
+ 
+nPairsR = table2cell(unique(cell2table([gene1,gene2]))); 
 fprintf('There are %d entries in the original data, %d pairs are unique\n', length(gene1), length(nPairsR))
-
+clear('nPairsR');
 
 % Strip whitespace:
 gene1 = strtrim(gene1);
@@ -73,11 +75,37 @@ PPIN = [gene1(keepEdge),gene2(keepEdge)];
 evidenceScore = evidenceScore(keepEdge);
 numInteractions = sum(keepEdge);
 
-nPairsC = table2cell(unique(cell2table(PPIN))); 
-fprintf('There are %d entries in the characterised data, %d pairs are unique\n', size(PPIN,1), length(nPairsC))
+[nPairsC,W] = unique(cell2table(PPIN), 'rows','stable'); 
+fprintf('There are %d entries in the characterised data, %d pairs are unique\n', size(PPIN,1), size(nPairsC,1))
+clear('nPairsC');
+% after assigning proteins to genes, some protein pairs (n=36540) become non-unique 
+% what to do in that case? 
+% take the pair with the highest evidence score; 
+DUP = setdiff(1:size(PPIN,1),W); 
+isDUP = false(length(evidenceScore),1); 
+fprintf('There are %d duplicate entries\n', length(DUP))
+fprintf('Select entry with highest evidence score\n')
+
+for d=1:length(DUP)
+    V = PPIN(DUP(d),:);
+    V1 = find(ismember(PPIN(:,1),V{1})); 
+    V2 = find(ismember(PPIN(:,2),V{2})); 
+    V12 = intersect(V1,V2); 
+    
+    % find non-max scores and save their index to exclude in isDUP
+    % if all scores are equal, this will select one fo them
+    [~, sI] = sort(evidenceScore(V12));
+    isDUP(V12(sI(2:end))) = 1;
+    fprintf('%d pair processed\n', d); 
+end
+
+% remove non-selected duplicates
+PPIN(isDUP,:) = []; 
+evidenceScore(isDUP) = [];
+numInteractions = length(evidenceScore); 
 
 if doWeighted
-    fprintf(1,'Weighted analysis: keeping all %u edges in the PPIN\n',numInteractions);
+    fprintf(1,'Weighted analysis: keeping all unique %u edges in the PPIN\n',numInteractions);
 else
     fprintf(1,'Filtered on evidence threshold %g -> %u edges in the PPIN\n',...
                         evidenceThreshold,numInteractions);
@@ -99,20 +127,20 @@ fprintf(1,'(saved to %s)\n',fileNameSave{2});
 % Convert gene names to indices:
 fprintf(1,['Constructing sparse list of edges (%ux%u);',...
             ' as indices across %u edges...\n'],numGenes,numGenes,numInteractions);
-%indx1 = zeros(numInteractions,1,'uint16');
-%indx2 = zeros(numInteractions,1,'uint16');
+indx1 = zeros(numInteractions,1,'uint16');
+indx2 = zeros(numInteractions,1,'uint16');
 if length(geneNames) > 2^16
     error('Problem storing indices as unsigned int16 data');
 end
 
-AdjPPI = zeros(numGenes, numGenes); 
+%AdjPPI = zeros(numGenes, numGenes); 
 for k = 1:numInteractions
     % There can only be one match (since geneNames contains unique entries)
-    indx1 = find(strcmp(PPIN{k,1},geneNames),1);
-    indx2 = find(strcmp(PPIN{k,2},geneNames),1);
+    indx1(k) = find(strcmp(PPIN{k,1},geneNames),1);
+    indx2(k) = find(strcmp(PPIN{k,2},geneNames),1);
     
-    AdjPPI(indx1, indx2) = double(evidenceScore(k));
 end
+
 save(fileNameSave{3},'indx1','indx2');
 fprintf(1,'Indices saved to %s\n',fileNameSave{3});
 clear('PPIN');
@@ -120,26 +148,32 @@ clear('PPIN');
 %-------------------------------------------------------------------------------
 % Create a sparse matrix containing all interactions:
 fprintf(1,'Generating a sparse matrix from the gene-matched indices:\n');
-AdjPPI = zeros(numGenes, numGenes); 
 
-for i=1:length(evidenceScore)
-    if doWeighted
-
-        AdjPPI(double(indx1(i)), double(indx2(i))) = double(evidenceScore(i));
-        
-    else
-        
-        AdjPPI(indx1(i), indx2(i)) = 1;
-        % sparse version generates evidence Scores>1000 for the last gene in the matrix - don't know why;
-        % AdjPPI = sparse(double(indx1),double(indx2),double(evidenceScore),numGenes,numGenes);
-        
-        %AdjPPI = sparse(double(indx1),double(indx2),1,numGenes,numGenes);
-        %fprintf(1,'Symmetrizing the sparse matrix...');
-        %AdjPPI = (AdjPPI | AdjPPI');
-        
-    end
-    
+if doWeighted
+    AdjPPI = sparse(double(indx1),double(indx2),double(evidenceScore),numGenes,numGenes);
+else
+    AdjPPI = sparse(double(indx1),double(indx2),1,numGenes,numGenes);
 end
+
+    
+% for i=1:length(evidenceScore)
+%     if doWeighted
+% 
+%         AdjPPI(double(indx1(i)), double(indx2(i))) = double(evidenceScore(i));
+%         
+%     else
+%         
+%         AdjPPI(indx1(i), indx2(i)) = 1;
+%         % sparse version generates evidence Scores>1000 for the last gene in the matrix - don't know why;
+%         % AdjPPI = sparse(double(indx1),double(indx2),double(evidenceScore),numGenes,numGenes);
+%         
+%         %AdjPPI = sparse(double(indx1),double(indx2),1,numGenes,numGenes);
+%         %fprintf(1,'Symmetrizing the sparse matrix...');
+%         %AdjPPI = (AdjPPI | AdjPPI');
+%         
+%     end
+%     
+% end
 fprintf(1,' Done.\n');
 
 % check if the matrix is symmetric
