0e5ea5fc7a2e21be4ede9f3ce700cf99520e276b
Aurina <aurina.arnatk@gmail.com>
2020-07-03 16:12:08 +1000

    only unique pairs of SNP-GENE are saved to file

--- DataProcessing/SNPannotation/map_SNPtoGene.R
+++ DataProcessing/SNPannotation/map_SNPtoGene.R
@@ -1,6 +1,6 @@
 library("biomaRt")
 library("dplyr")
-library("R.matlab")
+
 
 setwd("~/Google_drive/PostDoc/projects/GWASdrugs/data/GWASlists/")
 # load data that will be used for mapping: SNP-level and gene level
@@ -12,17 +12,17 @@ grch38 = useMart(biomart="ENSEMBL_MART_ENSEMBL", host="ensembl.org", path="/biom
 # For all disorders do clumping using 1000GPhase 3 dataset to keep independent SNPs;
 # the list for ADHD sataset contains SNPs in LD with each other, need to clump and keep only independent ones; 
 
-DISORDERS=c('ADHD', 'AUT', 'BIP2', 'IQSavage', 'MDD2', 'SCZ', 'DIABETIES')
+DISORDERS=c('ADHD', 'AUT', 'BIP2', 'IQSavage', 'MDD2', 'SCZ', 'DIABETES')
 pThr = 10^-5
 for (disorder in DISORDERS){
   
 # load the dataset 'AUT', 'BIP2' have independent SNPs in the original file
 if (disorder=='AUT' || disorder=='BIP2'){
-  fileName = sprintf('pgc%s.txt', disorder)}
+  fileName = sprintf('pgc%s.txt', disorder)
 # for others load clumped data
-else{
+} else {
   fileName = sprintf('pgc%s.clumped', disorder)}
-}
+
   
 GWASlist <- read.delim(fileName, sep="")
 
@@ -33,20 +33,31 @@ SNPlist = GWASlist$SNP
 keepSNP = grepl("rs", GWASlist$SNP, fixed = TRUE)
 SNPlistKeep = SNPlist[keepSNP]
 
+sprintf("%s: unique SNPs p<%e identified n=%d", fileName, pThr, length(SNPlistKeep))
 
 # extract information relevant for SNPs for a selected list
 SNPtable <- getBM(attributes = c("refsnp_id", "ensembl_gene_stable_id"), 
                 filters = "snp_filter",
                 values = SNPlistKeep, 
-                mart = grch38.snp)
+                mart = grch38.snp,   
+                useCache = FALSE, 
+                uniqueRows = TRUE)
 
 # extract information relevant for genes for a selected list
 GENEtable <- getBM(attributes = c("ensembl_gene_id", "external_gene_name"),
                 filters = "ensembl_gene_id", 
                 values =  SNPtable$ensembl_gene_stable_id, 
-                mart = grch38)
+                mart = grch38, 
+                useCache = FALSE, 
+                uniqueRows = TRUE)
 
 results <- merge(SNPtable,GENEtable, by.x = "ensembl_gene_stable_id", by.y="ensembl_gene_id", all.x=T)
-# keep only SNPs with all data in each row
-FINALresults = results[complete.cases(results), ]
+# keep only unique rows of the data wirh SNPs with all data in each row
+FINALresults = unique(results[complete.cases(results), 2:3])
+
+
+write.table(FINALresults, file = sprintf("pgc%s_genes.txt",disorder), 
+            sep = "\t", row.names = FALSE, quote=FALSE)
+
+sprintf("%s: unique SNPs identified n=%d", fileName, nrow(FINALresults))
 }
