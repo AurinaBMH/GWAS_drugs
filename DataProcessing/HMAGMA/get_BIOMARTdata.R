f1e881f13ecdf126a63745f08a93a60bb5f5cef1
Aurina <aurina.arnatk@gmail.com>
2020-07-08 13:13:19 +1000

    added package instalation

--- DataProcessing/HMAGMA/get_BIOMARTdata.R
+++ DataProcessing/HMAGMA/get_BIOMARTdata.R
@@ -1,4 +1,17 @@
 # get BIOMART file using querie
+# first, will need to install the corresponding packages (do it only once), uncomment if running on a different computer
+
+#if (!requireNamespace("BiocManager", quietly = TRUE))
+#  install.packages("BiocManager")
+#BiocManager::install()
+
+#BiocManager::install(c("GenomicFeatures", "AnnotationDbi"))
+#BiocManager::install(c("biomaRt"))
+
+# load libraries
+library("biomaRt")
+library("dplyr")
+
 setwd("~/Google_drive/PostDoc/projects/GWASdrugs/data/GWASlists")
 
 ensembl = useMart(biomart="ENSEMBL_MART_ENSEMBL", host="ensembl.org", path="/biomart/martservice", dataset="hsapiens_gene_ensembl")
