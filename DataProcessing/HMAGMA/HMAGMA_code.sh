db3b2c7b003787c0d77fac59a6d5570b30d40ded
Aurina <aurina.arnatk@gmail.com>
2020-07-08 10:52:52 +1000

    eMAGMA line added

--- DataProcessing/HMAGMA/HMAGMA_code.sh
+++ DataProcessing/HMAGMA/HMAGMA_code.sh
@@ -13,13 +13,18 @@ do
 ${WHEREISCODE}/magma --bfile ${WHEREIS1000G}/g1000_eur --gene-annot ${WHEREISANNOT}/${whatANNOT}.genes.annot --pval ${WHEREISGWAS}/pgc${DISORDER}.txt N=10000 --out ${WHEREISOUT}/pgc${DISORDER}_${whatANNOT}
 cp ${WHEREISOUT}/pgc${DISORDER}_${whatANNOT}.genes.out ${WHEREISOUT}/pgc${DISORDER}_${whatANNOT}_genes.txt
 
-# do eMAGMA mapping using eQTL-based annotations
+# do eMAGMA mapping using eQTL-based annotations send by Zac, these are based on the psychENCODE database
+# Options are chosen based on the example on github: https://github.com/eskederks/eMAGMA-tutorial.git
+
 # ###############Gene-based eMAGMA!############
+${WHEREISCODE}/magma --bfile ${WHEREIS1000G}/g1000_eur --gene-annot ${WHEREISANNOT}/pec_genes.annot --pval ${WHEREISGWAS}/pgc${DISORDER}.txt N=10000 --gene-settings adap-permp=10000 --out ${WHEREISOUT}/pgc${DISORDER}_eQTLpec_genes.txt
+# The --gene-settings flag also controls the settings for permutation-based empirical gene p-values,
+# using the fixed-permp or adap-permp modifiers to enable computation of an empirical p-value using
+# a fixed number of permutations or an adaptive permutation procedure respectively
 
-# ${WHEREISCODE}/magma --bfile ${WHEREISCODE}/magma --gene-annot Brain/Brain_Amygdala.genes.annot --pval ${WHEREISGWAS}/pgc${DISORDER}.txt N=10000 --gene-settings adap-permp=10000 --out Amygdala_emagma
+done
+
+done
+
 # gene names are in the ENSG00000000457 format, need to get entrez IDs or gene names for them
-# use BIOMART annotations to assign IDs to genes in matlab
-
-done
-
-done
+# use BIOMART annotations to assign IDs to genes in matlab, this is done using get_HMAGMAentrezIDs.m 
