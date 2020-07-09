1a24b50d7de46c2657a65e6eec181216969f7ec2
Aurina <aurina.arnatk@gmail.com>
2020-07-09 18:45:27 +1000

    GTEx-based eQTLs added to the list

--- DataProcessing/HMAGMA/get_HMAGMAentrezIDs.m
+++ DataProcessing/HMAGMA/get_HMAGMAentrezIDs.m
@@ -30,28 +30,40 @@ for D=1:length(Disorders)
     for A=1:length(whatANNOT)
         
         fileName = sprintf('data/GWASlists/GWASgenes/pgc%s_%s_genes.txt', Disorders{D}, whatANNOT{A});
-        if contains(whatANNOT{A}, 'eQTL')
-            mapLIST = importeMAGMAoutfile(fileName);
+        
+        %GTEx - based files have genes labeled with entrezIDs, other with stable IDs
+        isGTEx = strcmp(whatANNOT{A}, 'eQTLWhole_Blood') || strcmp(whatANNOT{A}, 'eQTLLiver') || strcmp(whatANNOT{A}, 'eQTLHeart_Left_Ventricle'); 
+        
+        if isGTEx
+            mapLIST = importeMAGMAGTExfile(fileName);
+            [~, INDmap, indENT] = intersect(mapLIST.GENE, entrezIDonly.entrezgene_id, 'stable'); 
         else
             mapLIST = importHMAGMAoutfile(fileName);
+            [~, INDmap, indENT] = intersect(mapLIST.GENE, entrezIDonly.ensembl_gene_id, 'stable'); 
         end
 
         fprintf('Processing disorder: %s, annotation %s\n', Disorders{D}, whatANNOT{A});
-        
-        [~, INDmap, indENT] = intersect(mapLIST.GENE, entrezIDonly.ensembl_gene_id, 'stable'); 
-        
+
         % select only overlapping genes from the table
         % in some cases one gene has multiple entrez IDs, probably related
         % to recent updates, hard to tell which one to choose, so we'll
         % keep gene names as well; 
-        mapLIST = mapLIST(INDmap,:); 
-        ENTREZID = entrezIDonly.entrezgene_id(indENT);
-        GENENAME = entrezIDonly.external_gene_name(indENT);
+        mapLIST = mapLIST(INDmap,:);
         
-        % add to table
+        if isGTEx
+            % for non-GTEx, give entrezID the contents of gene and add stable IDs
+            ENTREZID = mapLIST.GENE; 
+            mapLIST.GENE = entrezIDonly.ensembl_gene_id(indENT);
+        else
+            ENTREZID = entrezIDonly.entrezgene_id(indENT);
+        end
+        
+        GENENAME = entrezIDonly.external_gene_name(indENT);
         mapLIST = addvars(mapLIST,ENTREZID,GENENAME,'After','GENE');
-        % divided 
-        pThr = 0.05/size(mapLIST,1); 
+                    
+        
+        % divided
+        pThr = 0.05/size(mapLIST,1);
         fprintf('%d genes\n', sum(mapLIST.P<pThr));
         
         DISORDERlist.(whatANNOT{A}).(Disorders{D}) = mapLIST; 
