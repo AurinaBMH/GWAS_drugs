e31bf64ad7fcd092e0bbd55d9c2280387c898aa3
Aurina <aurina.arnatk@gmail.com>
2020-07-20 17:56:49 +1000

    script for checking some PPI properties

---
+++ DataProcessing/check_PPIproperties.m
@@ -0,0 +1,61 @@
+% check how many genes survive thresholding
+
+load('GWAS_disordersMAGMA.mat')
+
+mappings = {'MAGMAdefault', 'eQTLbrain'}; 
+disorders = fieldnames(DISORDERlist.MAGMAdefault); 
+
+pThr = 0.01; 
+        
+for i=1:length(mappings)
+    for j=1:length(disorders)
+        
+        mapLIST = DISORDERlist.(mappings{i}).(disorders{j}); 
+        pFDR = mafdr(mapLIST.P, 'BHFDR', true); 
+
+        fprintf('%s %s: %d genes\n', mappings{i}, disorders{j}, sum(pFDR<pThr));
+        
+    end
+end
+
+% visualise PPI network distances at different thresholds
+thrs = [400,600,900]; 
+figure;
+for k=1:length(thrs)
+    
+load(sprintf('PPI_HGNC_Dist_th%d.mat', thrs(k)))
+subplot(1,3,k); 
+numDis = length(find(isinf(distMatrix(:,1))));
+numGenes = size(distMatrix,1); 
+histogram(maskuHalf(distMatrix)); title(sprintf('PPI distances thr%d, %d/%d genes disconnected', thrs(k), numDis, numGenes))
+
+end
+
+
+% check the number of neighbors in PPI network for one disorder
+load('resultsTable_BIP2.mat')
+
+% Count disease genes that are X-step neighbors on the PPI network
+figure; 
+subplot(1,3,1); histogram(geneScores.PPI_eQTLbrain_th600.numPPIneighbors1); title('numPPIneighbors1')
+subplot(1,3,2); histogram(geneScores.PPI_eQTLbrain_th600.numPPIneighbors2); title('numPPIneighbors2')% this has right-skewed distribution
+subplot(1,3,3); histogram(geneScores.PPI_eQTLbrain_th600.numPPIneighbors3); title('numPPIneighbors3')
+
+
+
+figure; histogram(geneScores.PPI_eQTLbrain_th0.numPPIneighbors1); 
+figure; histogram(geneScores.PPI_eQTLbrain_th0.numPPIneighbors2);
+figure; histogram(geneScores.PPI_eQTLbrain_th0.numPPIneighbors3); 
+        
+
+figure; histogram(geneScores.PPI_eQTLbrain_th400.numPPIneighbors1); 
+figure; histogram(geneScores.PPI_eQTLbrain_th400.numPPIneighbors2); % this has normal distribution
+figure; histogram(geneScores.PPI_eQTLbrain_th400.numPPIneighbors3); 
+
+
+
+figure; histogram(geneScores.PPI_eQTLbrain_th900.numPPIneighbors1); 
+figure; histogram(geneScores.PPI_eQTLbrain_th900.numPPIneighbors2); % this has right-skewed distribution
+figure; histogram(geneScores.PPI_eQTLbrain_th900.numPPIneighbors3); 
+figure; histogram(geneScores.PPI_eQTLbrain_th900.numPPIneighbors4); 
+
