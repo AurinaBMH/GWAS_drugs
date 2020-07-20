16f76e302d77fa78b26ea75f8154919e3123fe95
Aurina <aurina.arnatk@gmail.com>
2020-07-20 17:57:14 +1000

    option to use FDR thresholded gene lists and save it to file

--- DataProcessing/GenerateResultsTables.m
+++ DataProcessing/GenerateResultsTables.m
@@ -3,7 +3,8 @@
 % for each:
 
 % Diseases with SNP info, etc.:
-whatDiseases = {'ADHD', 'AUT', 'MDD2', 'SCZ', 'BIP2', 'DIABETES', 'HF', 'AD'};
+whatDiseases = {'ADHD', 'MDD2', 'SCZ', 'BIP2', 'DIABETES', 'HF', 'AD'};
+whatThreshold='FDR'; 
 % load information on genes mapped using different methods
 load('GWAS_disordersMAGMA.mat')
 params = SetDefaultParams();
@@ -12,9 +13,9 @@ params = SetDefaultParams();
 numDiseases = length(whatDiseases);
 for k = 1:numDiseases
     whatDisease = whatDiseases{k};
-    geneScores = pipeline(DISORDERlist, whatDisease);
+    geneScores = pipeline(DISORDERlist, whatDisease, whatThreshold);
     % Save:
-    fileName = sprintf('resultsTable_%s.mat',whatDisease);
+    fileName = sprintf('resultsTable_%s_%s.mat',whatDisease, whatThreshold);
     fileName = fullfile('DataOutput',fileName);
     save(fileName,'geneScores');
     fprintf(1,'Saved results to %s!!!\n\n\n',fileName);
