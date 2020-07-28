98691905827b69a76c6c044332e9089aa35c67e1
Aurina <aurina.arnatk@gmail.com>
2020-07-28 18:06:09 +1000

    colours for different significance thresholds

--- Peripheral/plot_matrixValues.m
+++ Peripheral/plot_matrixValues.m
@@ -7,9 +7,11 @@ textStrings = strtrim(cellstr(textStrings));  % Remove any space padding
 textStrings = strrep(textStrings(:),'NaN','-'); % replace NaN with
 [x, y] = meshgrid(1:(size(values,2)), 1:(size(values,1)));  % Create x and y coordinates for the strings
 hStrings = text(x(:), y(:), textStrings(:), ...  % Plot the strings
-                'HorizontalAlignment', 'center', 'FontWeight', 'Bold');
-%textColors = zeros(size(image,2)*size(image,1),3); 
-textColors = repmat(values(:) > 0.05, 1, 3);  % Choose white or black for the text color of the strings so
+                'HorizontalAlignment', 'center', 'FontWeight', 'Bold'); 
+textColors = repmat(0.25,length(values(:)), 3); 
+diffCol = find(values(:) < 0.05);
+vals = repmat(1, length(diffCol),3); 
+textColors(diffCol,:) = vals;   % Choose white or black for the text color of the strings so
 set(hStrings, {'Color'}, num2cell(textColors, 2));  % Change the text colors
  
 end
