45e5044f05513107b28cf6d992741718ad76f660
Aurina <aurina.arnatk@gmail.com>
2020-07-29 15:53:21 +1000

    save plots

--- Analysis/selectHighestGWASvsDRUGmatch.m
+++ Analysis/selectHighestGWASvsDRUGmatch.m
@@ -2,16 +2,26 @@
 % combination that gives the strongest match between GWAS and drugs.
 clear all; close all; 
 
-load('resultsTable_ADHD_FDR.mat', 'geneScores')
-
-% select all available similarity types
-similarityTypes = setdiff(fieldnames(geneScores), {'gene', 'params'});
-
-% for PPI select 1 neighbor and mean and median measures;
-whatDiseases_GWAS = {'BIP2'};
-whatDiseases_Drug = 'BIP';
+% first run all diseases with itself
+whatDiseases_GWAS = {'ADHD', 'MDD2', 'SCZ', 'BIP2', 'DIABETES', 'HF'};
 whatNull = 'randomDisease';
-PPImeasures_names = {'numPPIneighbors1','percPPIneighbors1','weiPPIneighbors1','expWeiPPIneighbors1', 'numPPIneighbors2','percPPIneighbors2'}; 
 
-compareGWASvsDRUGmatches(whatDiseases_GWAS, whatNull, similarityTypes, PPImeasures_names, whatDiseases_Drug); 
+for d = 1:length(whatDiseases_GWAS)
+    % input requires cell
+    whatDisease_GWAS{1} =  whatDiseases_GWAS{d};
+    compareGWASvsDRUGmatches(whatDisease_GWAS, whatNull); 
+    
+    figureName = sprintf('figures/GWAS%s_vs_drug%s_%s', whatDisease_GWAS{1}, whatDisease_GWAS{1}, whatNull);
+    print(gcf,figureName,'-dpng','-r300');
+    
+end
 
+% for PPI select all 1 and 2 neighbor measures
+whatDiseases_GWAS = {'BIP2'};
+whatDiseases_Drug = 'SCZ';
+whatNull = 'randomDisease';
+
+compareGWASvsDRUGmatches(whatDiseases_GWAS, whatNull, whatDiseases_Drug); 
+figureName = sprintf('figures/GWAS%s_vs_drug%s_%s', whatDiseases_GWAS{1}, whatDiseases_Drug, whatNull);
+print(gcf,figureName,'-dpng','-r300');
+    
