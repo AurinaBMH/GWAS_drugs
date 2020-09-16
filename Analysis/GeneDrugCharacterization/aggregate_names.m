d5d7f3c9f5be00eac2bc5be2cb9eddc282e0b56a
Aurina <aurina.arnatk@gmail.com>
2020-09-16 10:37:27 +1000

    compile full measure names

--- Analysis/GeneDrugCharacterization/aggregate_names.m
+++ Analysis/GeneDrugCharacterization/aggregate_names.m
@@ -1,25 +1,17 @@
-function measureNames = aggregate_names(diseaseResultsP, similarityTypes, PPImeasures_names)
+function measureNames = aggregate_names(diseaseResultsP, similarityTypes, ALLmeasures)
 
 % for every non-nan value, get their measure name
-
-
-k=1;
-for s=1:length(similarityTypes)
-    if contains(similarityTypes{s},'PPI')
-        for p=1:length(PPImeasures_names)
-            
-            whatProperty = PPImeasures_names{p};
-            similarityTypes{s} = strrep(similarityTypes{s},'_',' '); 
-            measureNames{k} = [similarityTypes{s}, '.' whatProperty]; 
-            k=k+1;
-        end
-    else
-        if ~contains(similarityTypes{s},'Allen')
-            whatProperty = 'P';
-        elseif contains(similarityTypes{s},'Allen')
-            whatProperty = 'r';
-        end
-        measureNames{k} = [similarityTypes{s}, '.', whatProperty]; 
-        k=k+1;
+names = cell(length(similarityTypes), length(ALLmeasures)); 
+for i=1:length(similarityTypes)
+    for j=1:length(ALLmeasures)
+        names{i,j} = [similarityTypes{i}, '.', ALLmeasures{j}]; 
     end
+end
+
+remIND = isnan(diseaseResultsP); 
+N = string(names); 
+N(remIND) = 'NaN'; 
+INDrem = strcmp(N(:), 'NaN');
+measureNames = names(~INDrem); 
+
 end
