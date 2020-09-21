5ddaf1f3852ab08f4f46559a236550cc76b8ea25
Aurina <aurina.arnatk@gmail.com>
2020-09-21 16:39:40 +1000

    use testing drug set

--- Analysis/GeneDrugCharacterization/compare_crossDisorder_optimizedScores.m
+++ Analysis/GeneDrugCharacterization/compare_crossDisorder_optimizedScores.m
@@ -10,9 +10,9 @@ params = SetDefaultParams();
 whatNorm = params.whatNorm;
 
 
-[geneWeightsGWAS_ALL_train, drugScores_ord, similarityTypes, PPImeasures_names] = give_GWASandDRUG_scores(disorderTrain, whatMeasures);
+[geneWeightsGWAS_ALL_train, drugScores_train, similarityTypes, PPImeasures_names] = give_GWASandDRUG_scores(disorderTrain, whatMeasures);
 % select other disorder scores to compare to
-geneWeightsGWAS_ALL_test = give_GWASandDRUG_scores(disorderTest, whatMeasures);
+[geneWeightsGWAS_ALL_test, drugScores_test] = give_GWASandDRUG_scores(disorderTest, whatMeasures);
 
 % when different disorders are compared, remove all values that are NaN at least in one; 
 INDnanTrain = all(isnan(geneWeightsGWAS_ALL_train),1);
@@ -26,7 +26,7 @@ geneWeightsGWAS_ALL_test(:, INDnan) = [];
 % plot randomNullP-based p-values for each mapping method
 Dname = disorderTest(isstrprop(disorderTest,'alpha'));
 
-[diseaseResultsR, diseaseResultsP, ~,~, measureNames] = compareGWASvsDRUGmatches({disorderTest}, whatNull, Dname, PPImeasures_names, similarityTypes);
+[~, diseaseResultsP, ~,~, measureNames] = compareGWASvsDRUGmatches({disorderTest}, whatNull, Dname, PPImeasures_names, similarityTypes);
 close all;
 % replace p=0 to the lowest possible with this number of nulls to avoid InF
 % in plotting
@@ -58,16 +58,19 @@ end
 
 xlabel('-log10(P)');
 title(sprintf('%s trained on %s', disorderTest, disorderTrain));
-xlim([0 3.5])
+xlim([0 4])
 hold on;
 
 % apply linear regression
-mdl = fitlm(geneWeightsGWAS_ALL_train,drugScores_ord);
+mdl = fitlm(geneWeightsGWAS_ALL_train,drugScores_train);
 
 yTest = predict(mdl,geneWeightsGWAS_ALL_test);
 yTestNorm = normalizeScoreVector(yTest, whatNorm);
 
-Pval_comb = compare_to_null(disorderTest, yTestNorm, drugScores_ord, whatNull);
+Pval_comb = compare_to_null(disorderTest, yTestNorm, drugScores_test, whatNull);
+if Pval_comb==0
+    Pval_comb = 1/params.numNull;
+end
 pPLOT = -log10(Pval_comb);
 
 % plot randomNullP-based p-value for combined measure
