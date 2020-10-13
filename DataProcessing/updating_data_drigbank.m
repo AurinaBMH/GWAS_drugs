68d0c79f18df7e24efea43e2bd5868e93fb01096
Aurina <aurina.arnatk@gmail.com>
2020-10-13 11:46:52 +1100

    data to update

--- DataProcessing/updating_data_drigbank.m
+++ DataProcessing/updating_data_drigbank.m
@@ -1,3 +1,9 @@
 % reprocessing data using DrugBank data: 
 dataTable = give_drugTargets('active', 'drugbank'); 
 GenerateResultsTables
+
+% generate nulls
+generate_randomDrug_nulls('drugbank')
+
+% generate results
+
