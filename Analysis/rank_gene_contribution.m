88f363f4e750204e0e1ad20e24bcc2e49dd9ea07
Aurina <aurina.arnatk@gmail.com>
2020-10-22 18:16:10 +1100

    save output, include a table of ranked dot-products without comparing to null

--- Analysis/rank_gene_contribution.m
+++ Analysis/rank_gene_contribution.m
@@ -1,6 +1,6 @@
 % this function ranks each gene by its contribution to the matching between
 % GWAS and drug target
-function T = rank_gene_contribution(whatGWAS, whatDrugs, similarityType, whatNull)
+function [Tp, Tdot] = rank_gene_contribution(whatGWAS, whatDrugs, similarityType, whatNull)
 
 if nargin < 4
     whatNull = 'randomDrugR_all_drugbank'; 
@@ -9,7 +9,7 @@ end
 % load null data
 fileName = sprintf('nulls_5000_2020targets_%s.mat', whatNull); 
 load(fileName, 'whatDiseases_Treatment', 'RANDOMdrugs_treatment', 'geneNames');
-[~, INDnull] = intersect(whatDiseases_Treatment, whatDrugs);
+
 
 params = SetDefaultParams();
 whatDiseases_Treatment_ALL = params.whatDiseases_Treatment_ALL;
@@ -37,6 +37,7 @@ geneWeightsDRUG = drugScoresAll(ib,INDtreatment);
 
 % Combine nulls and real data on overlap
 [~,~,ir] = intersect(geneNames_matched, geneNames, 'stable');
+[~, INDnull] = intersect(whatDiseases_Treatment, whatDrugs);
 geneWeightsNull = RANDOMdrugs_treatment{INDnull}(ir,:); 
 
 numGenes = length(geneWeightsGWAS); 
@@ -47,6 +48,8 @@ numNulls = size(geneWeightsNull,2);
 % compare real vs null, get p-value
 % save p-value, rank based on p-values
 
+% for a different GWAS on the same drugs, everything is rescaled by GWAS
+% values, but the ranking is the same
 dotP = nan(numGenes,1); 
 dotP_null = nan(numGenes, numNulls); 
 p_val = nan(numGenes,1); 
@@ -62,8 +65,13 @@ for g=1:numGenes
 end
 [~, iP] = sort(p_val, 'ascend'); 
 % rank p_vals and give out a ranked table of all genes
-T = table; 
-T.Gene = geneNames(iP); 
-T.Pval = p_val(iP); 
+Tp = table; 
+Tp.Gene = geneNames(iP); 
+Tp.Pval = p_val(iP); 
+
+[~, iD] = sort(dotP, 'descend'); 
+Tdot = table; 
+Tdot.Gene = geneNames(iD); 
+Tdot.DotP = dotP(iD); 
 
 end
--- Analysis/run_rank_gene_contribution.m
+++ Analysis/run_rank_gene_contribution.m
@@ -3,10 +3,11 @@ clear all; close all;
 % rank genes for relevant pairs of GWAS vs drug on SNP position measure
 similarityType = 'MAGMAdefault';
 % these lists are pairs of GWASvsDrugs for ranking;
-whatGWAS = {'MDD2', 'SCZ', 'DIABETES'};
-whatDrugs = {'BIP', 'BIP', 'DIABETES'};
+whatGWAS = {'MDD2', 'SCZ', 'BIP2', 'DIABETES'};
+whatDrugs = {'BIP', 'BIP', 'BIP', 'DIABETES'}; 
 for i=1:length(whatDrugs)
-    T_SNPposition.(whatGWAS).(whatDrugs) = rank_gene_contribution(whatGWAS{i}, whatDrugs{i}, similarityType);
+    [T_SNPpositionP.(['GWAS_',whatGWAS{i}]).(['drug_',whatDrugs{i}]),...
+    T_SNPpositionD.(['GWAS_',whatGWAS{i}]).(['drug_',whatDrugs{i}])] = rank_gene_contribution(whatGWAS{i}, whatDrugs{i}, similarityType);
 end
 
 
@@ -17,8 +18,11 @@ whatGWAS = {'SCZ', 'BIP2', 'BIP2', 'DIABETES'};
 whatDrugs = {'BIP', 'BIP', 'SCZ', 'DIABETES'}; 
 
 for i=1:length(whatDrugs)
-    T_PPInetwork.(whatGWAS).(whatDrugs) = rank_gene_contribution(whatGWAS{i}, whatDrugs{i}, similarityType);
+    [T_PPInetworkP.(['GWAS_',whatGWAS{i}]).(['drug_',whatDrugs{i}]), ...
+    T_PPInetworkD.(['GWAS_',whatGWAS{i}]).(['drug_',whatDrugs{i}])]= rank_gene_contribution(whatGWAS{i}, whatDrugs{i}, similarityType);
 end
 
+save('DataOutput/rank_genes_example.mat', ...
+    'T_SNPpositionP', 'T_SNPpositionD', 'T_PPInetworkP', 'T_PPInetworkD'); 
 
 
