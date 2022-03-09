f4bf43b715946f573be9a6d75ab84cf7110f5547
Aurina <aurina.arnatk@gmail.com>
2022-03-09 11:01:08 +1100

    Instructions for updating results

--- updating_results.m
+++ updating_results.m
@@ -1,3 +1,6 @@
+% --------------------------------------------
+% Aggregating GWAS-based information
+% --------------------------------------------
 % updating data using new GWAS for MDD, BIP and SCZ
 
 % 1. run /Users/aurinaa/Documents/PostDoc/projects/GWASdrugs/code/DataProcessing/HMAGMA/HMAGMA_code_2022.sh 
@@ -8,3 +11,22 @@
 
 % 2.1 update gene IDs for MAGMA outputs and collate all results into a
 % single .mat file using save_MAGMAHresults() function; 
+
+% 3.1 creating results tables for each disorder using
+% GenerateResultsTables() function; This will create geneScores structure for each disorder. 
+% This step takes the longest to run; 
+
+% --------------------------------------------
+% Aggregating drug target information
+% --------------------------------------------
+% 1. Combine drug target information from .txt files into matlab format
+% dataTable = give_drugTargets('all', 'drugbank')
+
+% 2. Generate nulls for each disorder - 5000 drug-based vectors selecting X number of random drugs: 
+% generate_randomDrug_nulls('drugbank')
+
+% --------------------------------------------
+% Reproducing results
+% --------------------------------------------
+% Generate figures using: plot_Psych_figures.m
+% For non-psychiatric disorders: plot_Body_figures.m
