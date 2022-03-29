a5837798a2445d86ef1f5d46e9f110ea5202f592
Aurina <aurina.arnatk@gmail.com>
2022-03-29 17:12:30 +1100

    redundant code removed

--- DataProcessing/HMAGMA/HMAGMA_code_2022.sh
+++ DataProcessing/HMAGMA/HMAGMA_code_2022.sh
@@ -6,11 +6,16 @@ WHEREISANNOT='/Users/aurinaa/Documents/PostDoc/projects/GWASdrugs/data/GWASlists
 
 # SCZ3 GWAS contains a line with missing values, it needed to be removed for MAGMA to run
 # line: 21 rs148878475 9648204 C T 0.9878 0.9845 0.1375 7.5732 1.0014 0.0432 0
+# MDD2 dataset: on Chr21 there are 61 SNPs that have only 12 instead of 19 fields. This is because only one cohort
+# contributed these SNPs. Remove 61 lines from 21 chromosome,if not removed, will break MAGMA
+# cat ${WHEREISGWAS}/pgcMDD2.txt | awk '(NF==19)' > ${WHEREISGWAS}/pgcMDD2.txt
+# DIABETES GWAS contains a line with a very low p-value, that is read as non-number;
+# it needed to be removed for MAGMA to run
+# line: 10 114758349 rs7903146 T C 0.291584512494863 0.3059 0.0077 1.33e-347 597475
 
-# for DISORDER in 'ADHD' 'MDD2' 'SCZ' 'DIABETES' 'BIP2' 'HF' 'AD' 'AUT' 'IBD_Liu' 'BIP2011' 'RA' 'IBD_Lange' 'BIPandSCZ'
-# 'MDD3' 'ADHD3' 'SCZ3' 'BIP3' 'DIABETES' 'HF' 'RA' 'IBD'
-
-for DISORDER in 'MDD3' 'ADHD3' 'SCZ3' 'BIP3' 'DIABETES' 'HF' 'RA' 'IBD'
+# latest dataset: 'MDD3' 'ADHD3' 'SCZ3' 'BIP3' 'DIABETES' 'HF' 'RA' 'IBD'
+# older datasets for psychiatric disorders: 'MDD2' 'ADHD' 'SCZ' 'BIP2'
+for DISORDER in 'MDD3' 'ADHD3' 'SCZ3' 'BIP3' 'DIABETES' 'HF' 'RA' 'IBD' 'MDD2' 'ADHD' 'SCZ' 'BIP2'
 do
 # with p-values
 # 1. use different HMAGMA annotations
